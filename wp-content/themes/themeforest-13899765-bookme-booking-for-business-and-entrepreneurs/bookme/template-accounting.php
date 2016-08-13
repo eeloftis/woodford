cc636b804b5b903ef724db1b6f7d13ce35dcac2c
Ema Loftis <ema@Emas-MBP.hsd1.ma.comcast.net>
2016-08-12 22:37:19 -0400

    swapped order

--- wp-content/themes/themeforest-13899765-bookme-booking-for-business-and-entrepreneurs/bookme/template-accounting.php
+++ wp-content/themes/themeforest-13899765-bookme-booking-for-business-and-entrepreneurs/bookme/template-accounting.php
@@ -11,13 +11,13 @@ get_header();
 				get_template_part('template-parts/section/about');
 				get_template_part('template-parts/section/services');
 
-			 the_content();
+			get_template_part('template-parts/section/parallax');
 				get_template_part('template-parts/section/news');
 				get_template_part('template-parts/section/facts');
 				get_template_part('template-parts/section/testi');
 				get_template_part('template-parts/section/about', 'details');
 				get_template_part('template-parts/section/clients');
-				get_template_part('template-parts/section/parallax');
+
 
 			endwhile;
 		else :
