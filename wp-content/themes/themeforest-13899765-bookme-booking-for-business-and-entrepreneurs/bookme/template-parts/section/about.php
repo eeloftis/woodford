384ed66c450be8695ea7f819bc3cf3be8b1ac129
Ema Loftis <ema@Emas-MBP.hsd1.ma.comcast.net>
2016-08-13 05:15:27 -0400

    more padding

--- wp-content/themes/themeforest-13899765-bookme-booking-for-business-and-entrepreneurs/bookme/template-parts/section/about.php
+++ wp-content/themes/themeforest-13899765-bookme-booking-for-business-and-entrepreneurs/bookme/template-parts/section/about.php
@@ -36,7 +36,7 @@
 					?>
 				</div>
 			</div>
-			<div class="col-md-6 col-sm-6">
+			<div class="col-md-5 col-md-offset-1 col-sm-6">
 				<div class="row">
 					<div class="col-md-11 col-md-offset-1 col-sm-11 col-sm-offset-1">
 						<div class="col-md-6 col-sm-6">
@@ -52,7 +52,7 @@
 								?>
 							</div>
 						</div>
-						<div class="col-md-6 col-sm-6">
+						<div class="col-md-5 col-sm-5">
 							<div class="row about-item">
 								<?php
 									$img3_url = get_post_meta(get_the_ID(), '_BookmeMB_about_image3', true);
