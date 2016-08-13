8ec70b5ddacfc86d52263f37ef43a4c1beb3e4a3
Ema Loftis <ema@Emas-MBP.hsd1.ma.comcast.net>
2016-08-12 22:32:40 -0400

    trying to add connect

--- wp-content/themes/themeforest-13899765-bookme-booking-for-business-and-entrepreneurs/bookme/template-accounting.php
+++ wp-content/themes/themeforest-13899765-bookme-booking-for-business-and-entrepreneurs/bookme/template-accounting.php
@@ -1,15 +1,17 @@
 <?php
 /**
- * Template Name: Accounting Template 
+ * Template Name: Accounting Template
  *
  */
-get_header(); 
-		if ( have_posts() ) : 
-			while ( have_posts() ) : the_post(); 
-			
-				get_template_part('template-parts/section/slider'); 
+get_header();
+		if ( have_posts() ) :
+			while ( have_posts() ) : the_post();
+
+				get_template_part('template-parts/section/slider');
 				get_template_part('template-parts/section/about');
 				get_template_part('template-parts/section/services');
+
+			 the_content();
 				get_template_part('template-parts/section/news');
 				get_template_part('template-parts/section/facts');
 				get_template_part('template-parts/section/testi');
@@ -21,5 +23,5 @@ get_header();
 		else :
 			get_template_part( 'content', 'none' );
 		endif;
-		
+
 get_footer(); ?>
