2d30cd4dd5eee2e6a7271227329db7dcf4cfa435
Ema Loftis <eloftis@wellesley.edu>
2016-08-23 18:56:10 -0400

    fixing maps a bit

--- wp-content/themes/themeforest-13899765-bookme-booking-for-business-and-entrepreneurs/bookme/template-accounting.php
+++ wp-content/themes/themeforest-13899765-bookme-booking-for-business-and-entrepreneurs/bookme/template-accounting.php
@@ -45,12 +45,12 @@ function initMap() {
         var mapDiv = document.getElementById("ny-map");
         var map = new google.maps.Map(mapDiv, {
           center: {lat: 40.712, lng: -74.005},
-          zoom: 8
+          zoom: 7
         });
 				var othermapDiv = document.getElementById("miami-map");
 				var othermap = new google.maps.Map(othermapDiv, {
 					center: {lat: 25.761, lng: -80.191},
-					zoom: 8
+					zoom: 9
 				});
      }
 </script>
