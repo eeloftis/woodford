87c4778848732ddcbe697ec86d082e3e79d0e204
Ema Loftis <eloftis@wellesley.edu>
2016-08-23 19:33:51 -0400

    added google translate

--- wp-content/themes/themeforest-13899765-bookme-booking-for-business-and-entrepreneurs/bookme/header.php
+++ wp-content/themes/themeforest-13899765-bookme-booking-for-business-and-entrepreneurs/bookme/header.php
@@ -23,6 +23,12 @@
 ?>
 </head>
 
+<div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'es', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
 <body <?php body_class(); ?>>
     <div id="wrapp">
 
