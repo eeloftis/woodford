a9463cfc3af2f73651805419e0aa61ee64dfebb4
Ema Loftis <eloftis@wellesley.edu>
2016-08-23 19:12:11 -0400

    Testing map

--- wp-content/themes/themeforest-13899765-bookme-booking-for-business-and-entrepreneurs/bookme/js/bookme.js
+++ wp-content/themes/themeforest-13899765-bookme-booking-for-business-and-entrepreneurs/bookme/js/bookme.js
@@ -410,6 +410,8 @@ $("#locate").click(function() {
 	}
 	else {
 		$("#locate").addClass('open');
+		google.maps.event.trigger(map, 'resize');
+			google.maps.event.trigger(othermap, 'resize');
 		$("#the-maps").show();
 	}
 });
